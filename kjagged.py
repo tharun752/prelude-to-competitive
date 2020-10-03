6eb06ab120fd70cd4c8a2129ea0e766908385b0c
tharun balanagu <tharunallzwell@gmail.com>
2020-10-03 21:15:00 +0530

    init

--- kjagged.py
+++ kjagged.py
@@ -1,20 +1,16 @@
 import math
 def gpf(n):
-    mx=0
+    
     bl=1
     while(n%2==0):
-        mx=2
-        bl=0
-        n//=2
+        return 2
     for i in range(3,int(math.sqrt(n))+1,2):
         if(bl):
             while(n%i==0):
-                mx=i
-                break
-                n//=i
-    if(2<n and mx<n):
-        mx=n
-    return mx
+                return i
+    if(2<n ):
+        return n
+    
 for _ in range(int(input())):
     num=int(input())
     k=int(input())
